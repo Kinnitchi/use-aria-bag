957cf046524cdbf07afd47baa41d84986dc7baa2
Kinnitchi <igorivieira97@gmail.com>
2026-03-30 22:18:38 -0300

    feat(auth): add additional address fields and phone number to auth_user table

---
+++ drizzle/0002_tidy_quasar.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "auth_user" ADD COLUMN "phone" text;--> statement-breakpoint
+ALTER TABLE "auth_user" ADD COLUMN "address_zip_code" text;--> statement-breakpoint
+ALTER TABLE "auth_user" ADD COLUMN "address_street" text;--> statement-breakpoint
+ALTER TABLE "auth_user" ADD COLUMN "address_number" text;--> statement-breakpoint
+ALTER TABLE "auth_user" ADD COLUMN "address_complement" text;--> statement-breakpoint
+ALTER TABLE "auth_user" ADD COLUMN "address_neighborhood" text;--> statement-breakpoint
+ALTER TABLE "auth_user" ADD COLUMN "address_city" text;--> statement-breakpoint
+ALTER TABLE "auth_user" ADD COLUMN "address_state" text;
